b9df0de6b385ef6566f2e8e6b46e40d6cad6d91e
DEICIDIA <43384189+DEICIDIA@users.noreply.github.com>
2020-02-14 16:20:46 +0100

    Add files via upload

--- taquinRngBlck.py
+++ taquinRngBlck.py
@@ -5,7 +5,7 @@ taquin = [7,3,4,
           8,2,6]
 
 nb = 0
-exMoov = 0
+mouvementPrecedent = 0
 
 def legalMoove(tab):
      index = tab.index(9)
@@ -34,24 +34,57 @@ def check(list):
     else:
         return False
 
+
+def choixMouvement(list):
+    moov = random.choice(list)
+    return moov
+
+def afficherTaquin():
+    tab1 = []
+    tab2 = []
+    tab3 = []
+    tab1.append(taquin[0])
+    tab1.append(taquin[1])
+    tab1.append(taquin[2])
+    tab2.append(taquin[3])
+    tab2.append(taquin[4])
+    tab2.append(taquin[5])
+    tab3.append(taquin[6])
+    tab3.append(taquin[7])
+    tab3.append(taquin[8])
+    print(tab1)
+    print(tab2)
+    print(tab3)
+    print("nombre bien placé :", etatTaquin(taquin))
+    print("liste de mouvements possible :", mouvementPossible)
+    print("mouvement choisi :", mouvementChoisi)
+    print()
+
 def swapPositions(list, pos1, pos2):
     list[pos1], list[pos2] = list[pos2], list[pos1]
     return list
 
+def etatTaquin(list):
+    count = 0
+    for s in range(0,len(list)):
+        if s+1 == list[s]:
+            count += 1
+    return count
 
 while check(taquin) == False:
     posX = taquin.index(9)
-    listeMoov = legalMoove(taquin)
+    mouvementPossible = legalMoove(taquin)
 
     if nb == 0:
-        moovPossible = random.choice(listeMoov)
+        mouvementChoisi = choixMouvement(mouvementPossible)
     elif nb > 0:
-        listeMoov.remove(exMoov*-1)
-        moovPossible = random.choice(listeMoov)
+        mouvementPossible.remove(mouvementPrecedent*-1)
+        mouvementChoisi = choixMouvement(mouvementPossible)
 
-    swapPositions(taquin, posX, posX + moovPossible)
-    exMoov = moovPossible
+    swapPositions(taquin, posX, posX + mouvementChoisi)
+    mouvementPrecedent = mouvementChoisi
     nb += 1
+    afficherTaquin()
 
-
+print()
 print(nb)
