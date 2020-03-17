fc6c1e9d6ecfb5fc6aa7e70bf532d71159310d98
DEICIDIA <43384189+DEICIDIA@users.noreply.github.com>
2020-03-17 18:33:17 +0100

    Update RngNxN.py

--- RngNxN.py
+++ RngNxN.py
@@ -3,10 +3,9 @@ import math
 
 trou = 9
 
-taquin = [1,2,3,4,
-          5,6,7,8,
-          9,10,11,12,
-          13,14,16,15]
+taquin = [1,2,3,
+          4,5,6,
+          7,8,9]
 
 taquinTampon = taquin.copy()
 
