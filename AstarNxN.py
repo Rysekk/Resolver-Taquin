aab5ca33ba177be2919287c25a3d4e684c6c6cd7
DEICIDIA <43384189+DEICIDIA@users.noreply.github.com>
2020-03-20 19:01:41 +0100

    Update AstarNxN.py

--- AstarNxN.py
+++ AstarNxN.py
@@ -3,15 +3,11 @@ import math
 
 trou = 9
 
-taquin = [4,1,2,
-          7,9,3,
-          8,5,6]
+taquin = [1,2,3,
+          4,5,6,
+          9,7,8]
 
-nbSwap = 0
-mouvementPrecedent = 0
 n = math.sqrt(len(taquin))
-
-
 
 def legalMoove(tab): # renvoi une liste de mouvements possible en fonction de la position du trou
     index = tab.index(trou)
@@ -34,11 +30,11 @@ def legalMoove(tab): # renvoi une liste de mouvements possible en fonction de la
     else:  # centre
         return [-3, -1, 1, 3]
 
-def choixMouvement(list): # choix meilleurs mouvement entre h1 et h2
+def choixMouvement(list): # choix meilleurs mouvement
     plusCourt = testSucc(taquin,list[0])
     meilleurMouv = list[0]
     for s in range(0,len(list)):
-        if testSucc(taquin,list[s]) > plusCourt:
+        if testSucc(taquin,list[s]) < plusCourt:
             meilleurMouv = list[s]
             plusCourt = testSucc(taquin,list[s])
     return meilleurMouv
@@ -47,28 +43,34 @@ def swapPositions(list, pos1, pos2):
     list[pos1], list[pos2] = list[pos2], list[pos1]
     return list
 
-def nombreElemOk(list): #h2
-    count = 0
+def inversion(list):
+    sum = 0
+    for s in range(0,len(list)):
+        for i in range(s+1,len(list)):
+            if list[s] > list[i]:
+                if list[s] != 9:
+                    sum += 1
+    return sum
+
+def desordre(list): #h2
+    count = len(taquin)
     for s in range(0, len(list)):
         if s+1 == list[s]:
-            count += 1
+            count -= 1
     return count
 
 def distOrigine(nb):
     target = (nb - 1)
     pos = taquin.index(nb)
     dist = 0
-
     if pos == target:
         return dist
-
     elif pos < target:
         while target - 3 >= pos:
             dist += 1
             target -= 3
         dist += abs(pos - target)
         return dist
-
     elif pos > target:
         while pos - 3 >= target:
             dist += 1
@@ -86,67 +88,78 @@ def testSucc(list, mouv): #test h1 + h2
     tab = list.copy()
     posX = tab.index(trou)
     swapPositions(tab, posX, mouv + posX)
-    return SommeDist(tab) + nombreElemOk(tab)
+    return SommeDist(tab) + nombreElemOk(tab) + inversion(tab)
+
+class arbre:
+
+    def __init__(self):
+        self.nbSwap = 0
+        self.frontiere = []
+
+frontiere = []
+explorer = []
+
+class noeud:
+    mouvement = []
+    def __init__(self, list, nbSwap , mouv, pere, generation):
+        self.tab = list
+        self.nb = nbSwap
+        self.pere = pere
+        self.mouvement = mouv
+        self.generation = generation+1
+        self.heuristic = desordre(self.tab) + self.nb + self.generation
+    def __repr__(self):
+        print(str(self.h()))
+    def h(self):
+        return self.heuristic
+    def taquin(self):
+        return self.tab
+    def mouv(self):
+        return self.mouvement
+    def getGeneration(self):
+        return self.generation
+    def expend(self):
+        mouvementPossible = legalMoove(self.tab)
+        for s in range(0, len(mouvementPossible)):
+            tab = self.tab.copy()
+            posX = tab.index(trou)
+            mouv = mouvementPossible[s]
+            swapPositions(tab, posX, mouv + posX)
+            nouveauNoeud = noeud(tab, nbSwap, mouv, self, self.generation)
+            if frontiere == []:
+                frontiere.append(nouveauNoeud)
+            else:
+                for s in range(0,len(frontiere)):
+                    if frontiere[s].h() >= nouveauNoeud.h():
+                        frontiere.insert(s,nouveauNoeud)
+                        break
+        explorer.append(self)
+        if self.generation >= 1:
+            frontiere.remove(self)
+
+    def etatBut(self):
+        if desordre(self.tab) == 0:
+            return True
+        else:
+            False
+    def getPere(self):
+        return self.pere
 
 
-print("taquin : ",taquin)
-print("nombre elem ok : ",nombreElemOk(taquin))
-print("trou : ", trou)
-print("position trou : ", taquin.index(trou))
-print("taille : ", n)
+nbSwap = 0
+root = noeud(taquin,nbSwap,[], None, -1)
+root.expend()
+print("racine : ", root.taquin(), " heuristique : ", root.h(), " gen :", root.getGeneration())
+print()
+for s in range(0,len(frontiere)):
+    print("frontière : ",frontiere[s].taquin(), " heuristique : ", frontiere[s].h(), " gen :" , frontiere[s].getGeneration())
 
-
-
-def afficherTaquin():
-    tab1 = []
-    tab2 = []
-    tab3 = []
-
-    tab1.append(taquin[0])
-    tab1.append(taquin[1])
-    tab1.append(taquin[2])
-    tab2.append(taquin[3])
-    tab2.append(taquin[4])
-    tab2.append(taquin[5])
-    tab3.append(taquin[6])
-    tab3.append(taquin[7])
-    tab3.append(taquin[8])
-
-    print(tab1)
-    print(tab2)
-    print(tab3)
-    print()
-
-
-chemin = []
-
-while nombreElemOk(taquin) !=  len(taquin):
-        posX = taquin.index(trou)
-        mouvementPossible = legalMoove(taquin)
-
-        if nbSwap == 0:
-            mouvementChoisi = choixMouvement(mouvementPossible)
-        elif nbSwap > 0:
-            mouvementPossible.remove(mouvementPrecedent * -1)
-            mouvementChoisi = choixMouvement(mouvementPossible)
-
-        mouvementPrecedent = mouvementChoisi
-        chemin.append(mouvementChoisi)
-        posElementChoisi = posX + mouvementChoisi
-        swapPositions(taquin, posX, posElementChoisi)
-        nbSwap += 1
-
-        print()
-        afficherTaquin()
-        print("étapes nb : ", nbSwap)
-        print("somme distance du taquin : ", SommeDist(taquin))
-        print("nb elements ok : ", nombreElemOk(taquin))
-        print("choix possible : ", legalMoove(taquin))
-        print("")
-
-
-
-
+frontiere[0].expend()
 
 print()
-print("chemin : ", chemin)
+for s in range(0,len(frontiere)):
+    print("frontière : ", frontiere[s].taquin(), " heuristique : ", frontiere[s].h(), " gen : ", frontiere[s].getGeneration())
+
+print()
+for s in range(0,len(explorer)):
+    print("explorée : ",explorer[s].taquin(), " heuristique : ", explorer[s].h(), " gen : ", explorer[s].getGeneration())
