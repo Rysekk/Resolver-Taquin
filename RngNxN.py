16c2d868e6741c46c8bed179bb4092972df49bc9
DEICIDIA <43384189+DEICIDIA@users.noreply.github.com>
2020-03-24 19:33:19 +0100

    Update RngNxN.py

--- RngNxN.py
+++ RngNxN.py
@@ -1,161 +1,100 @@
 import random
-import math
+x = 9
 
-trou = 9
 
-taquin = [1,2,3,
-          4,5,6,
-          9,7,8]
 
-taquinTampon = taquin.copy()
+taquin = [0,3,5,4,2,1,6,7,8]
+
+taquin2 = taquin.copy()
 
 nbSwap = 0
 mouvementPrecedent = 0
-taillePlusCourt = 10000000
+plusCourt = []
+wtf = []
+taillePlusCourt = 1000000000
 
-cheminActuel = []
-meilleurChemin = []
+def legalMoove(tab):
+     index = tab.index(0)
+     if index == 0:
+         return [1,3]
+     if index == 1:
+         return [-1,3,1]
+     if index == 2:
+         return [-1,3]
+     if index == 3:
+         return [-3,1,3]
+     if index == 4:
+         return  [-3,-1,1,3]
+     if index == 5:
+         return [-3,-1,3]
+     if index == 6:
+         return [-3,1]
+     if index == 7:
+         return [-3,-1,1]
+     if index == 8:
+         return [-3,-1]
 
-n = math.sqrt(len(taquin))
-profondeurRecherche = 30
+def choixMouvement(list):
+    wtf = random.choice(list)
+    return wtf
 
-
-
-def legalMoove(tab): # renvoi une liste de mouvements possible en fonction de la position du trou 
-    index = tab.index(trou)
-    if index == 0:  # coin haut gauche
-        return [1, 3]
-    if index == (n - 1):  # coin haut droit
-        return [-1, 3]
-    if index == ((n * n) - 1) - (n - 1):  # coin bas gauche
-        return [-3, 1]
-    if index == (n * n) - 1:  # coin bas droit
-        return [-3, -1]
-    if 0 < index < (n - 1):  # bordure haute
-        return [-1, 3, 1]
-    if ((n * n) - 1) - (n - 1) < index < (n * n) - 1:  # bordure basse
-        return [-3, -1, 1]
-    if index % n == 0:  # bordure gauche
-        return [-3, 1, 3]
-    if index % n == (n - 1):  # bordure droite
-        return [-3, -1, 3]
-    else:  # centre
-        return [-3, -1, 1, 3]
-
-def choixMouvement(list): # choix meilleurs mouvement entre h1 et h2 
-    plusCourt = testSucc(taquin,list[0])
-    meilleurMouv = list[0]
-    for s in range(0,len(list)):
-        print(testSucc(taquin,list[s]))
-        if testSucc(taquin,list[s]) < plusCourt:
-            meilleurMouv = list[s]
-            plusCourt = testSucc(taquin,list[s])
-    return meilleurMouv
-
-def swapPositions(list, pos1, pos2):
-    list[pos1], list[pos2] = list[pos2], list[pos1]
-    return list
-
-def nombreElemOk(list): #h2
-    count = 0
-    for s in range(0, len(list)):
-        if s+1 == list[s]:
-            count += 1
-    return count
-
-def SommeDist(list): #h1
-    somme = 0
-    for s in range(0,len(list)):
-        somme += distOrigine(list[s])
-    return somme
-
-def testSucc(list, mouv): #test h1 + h2 
-    tab = list.copy()
-    posX = tab.index(trou)
-    swapPositions(tab, posX, mouv + posX)
-    return SommeDist(tab) + nombreElemOk(tab)
-
-def distOrigine(nb):
-    target = (nb - 1)
-    pos = taquin.index(nb)
-    dist = 0
-    if pos == target:
-        return dist
-    elif pos < target:
-        while target - 3 > pos:
-            dist += 1
-            target -= 3
-        dist += abs(pos - target)
-        return dist
-    elif pos > target:
-        while pos - 3 > target:
-            dist += 1
-            pos -= 3
-        dist += abs(pos - target)
-        return dist
-
-print("taquin : ",taquin)
-print("nombre elem ok : ",nombreElemOk(taquin))
-print("trou : ", trou)
-print("position trou : ", taquin.index(trou))
-print("taille : ", n)
-print("taille max recherche : ",profondeurRecherche)
-print(legalMoove(taquin))
-print(choixMouvement(legalMoove(taquin)))
-
-def afficherTaquin():
+def afficherTaquin(list):
     tab1 = []
     tab2 = []
     tab3 = []
-
-    tab1.append(taquin[0])
-    tab1.append(taquin[1])
-    tab1.append(taquin[2])
-    tab2.append(taquin[3])
-    tab2.append(taquin[4])
-    tab2.append(taquin[5])
-    tab3.append(taquin[6])
-    tab3.append(taquin[7])
-    tab3.append(taquin[8])
-
+    tab1.append(list[0])
+    tab1.append(list[1])
+    tab1.append(list[2])
+    tab2.append(list[3])
+    tab2.append(list[4])
+    tab2.append(list[5])
+    tab3.append(list[6])
+    tab3.append(list[7])
+    tab3.append(list[8])
     print(tab1)
     print(tab2)
     print(tab3)
     print()
 
-for s in range(0, 700000):
+def swapPositions(list, pos1, pos2):
+    list[pos1], list[pos2] = list[pos2], list[pos1]
+    return list
 
-    while nombreElemOk(taquin) != len(taquin):
-        posX = taquin.index(trou)
+def nombreElemOk(list):
+    count = 0
+    for s in range(0,len(list)):
+        if s == list[s]:
+            count += 1
+    return count
+
+for s in range(0,50000):
+    while nombreElemOk(taquin) != 9:
+        posX = taquin.index(0)
         mouvementPossible = legalMoove(taquin)
-
         if nbSwap == 0:
             mouvementChoisi = choixMouvement(mouvementPossible)
         elif nbSwap > 0:
-            mouvementPossible.remove(mouvementPrecedent * -1)
+            mouvementPossible.remove(mouvementPrecedent*-1)
             mouvementChoisi = choixMouvement(mouvementPossible)
 
-        mouvementPrecedent = mouvementChoisi
-        cheminActuel.append(mouvementChoisi)
+        plusCourt.append(mouvementChoisi)
         posElementChoisi = posX + mouvementChoisi
         swapPositions(taquin, posX, posElementChoisi)
+        mouvementPrecedent = mouvementChoisi
         nbSwap += 1
-
         if nbSwap > taillePlusCourt:
             break
-        if nbSwap > profondeurRecherche:
+        if nbSwap > 50:
             break
-
-    if (nbSwap < taillePlusCourt) & (nbSwap != profondeurRecherche + 1):
-        print(cheminActuel)
-        print("taille :", nbSwap)
-        print("nombre d'essai pour solution : ", s)
+    if nbSwap < taillePlusCourt:
+        print(plusCourt)
         taillePlusCourt = nbSwap
-        meilleurChemin = []
-        meilleurChemin = cheminActuel.copy()
+        wtf = []
+        wtf = plusCourt.copy()
     else:
-        cheminActuel = []
-
+        plusCourt = []
     nbSwap = 0
-    taquin = taquinTampon.copy()
+    taquin = taquin2.copy()
+
+print(taillePlusCourt)
 
